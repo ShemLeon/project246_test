b7d42c702c2d13479f948ba6c9496e5b5826fafa
ShemLeon <shemiakinleonid@gmail.com>
2025-09-09 23:34:41 +0300

    Initial commit

---
+++ app/src/main/java/com/leoevg/project246/presentation/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.leoevg.project246.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.leoevg.project246.R
+import com.leoevg.project246.ui.theme.SplashScreen
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreen(
+                onStartClick = {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+            )
+        }
+    }
+}
