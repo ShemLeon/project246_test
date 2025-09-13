1211024b0c89220c0a64e20d07e28ef0596aab1c
ShemLeon <shemiakinleonid@gmail.com>
2025-09-13 17:30:36 +0300

    WaveformBar.kt

--- app/src/main/java/com/leoevg/project246/ui/theme/PlayerScreen.kt
+++ app/src/main/java/com/leoevg/project246/ui/theme/PlayerScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -206,6 +207,37 @@ fun PlayerScreen(
                 )
             }
 
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Center)
+                    .padding(horizontal = 24.dp)
+                    .padding(top = 320.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WaveformBar(
+                    values = waveform,
+                    progress = waveformProgress,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                ) { percent ->
+                    val seek = (percent * duration).toLong()
+                    exoPlayer.seekTo(seek)
+                    elapsed=seek
+                    waveformProgress = percent
+                }
+                Row (
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 6.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+
+                }
+            }
+
+
             Row(
                 Modifier
                     .fillMaxWidth()
@@ -240,12 +272,14 @@ fun PlayerScreen(
                 }
 
                 //play-pause button
-                IconButton(onClick = {
-                    if (exoPlayer.isPlaying) exoPlayer.pause() else exoPlayer.play()
-                },
+                IconButton(
+                    onClick = {
+                        if (exoPlayer.isPlaying) exoPlayer.pause() else exoPlayer.play()
+                    },
                     modifier = Modifier
                         .size(64.dp)
-                        .background(Color.White, shape = CircleShape)) {
+                        .background(Color.White, shape = CircleShape)
+                ) {
                     Icon(
                         painterResource(if (isPlaying) R.drawable.baseline_pause_24 else R.drawable.baseline_play_arrow_24),
                         contentDescription = null,
